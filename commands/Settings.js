e66dda6afca04ebde666fa18b3ac233af6eb4bcf
saviya2009 <126062795+saviya2009@users.noreply.github.com>
2024-01-15 21:25:05 +0530

    Update Settings.js

--- commands/Settings.js
+++ commands/Settings.js
@@ -40,72 +40,69 @@ cmd({
             ];
             let buttonMessage = {
                 image: {
-                    url: 'https://telegra.ph/file/a6b9bbde7feaa92c69c7b.jpg',
+                    url: 'hthttps://telegra.ph/file/888c0612dc54eb7f5bacd.jpg',
                 },
 
                 caption: `
 
-*⦁──🎃𝙎𝙀𝙏𝙏𝙄𝙉𝙂 𝙇𝙄𝙎𝙏📍──⦁*
+*⦁──☑𝙎𝙀𝙏𝙏𝙄𝙉𝙂 𝙇𝙄𝙎𝙏☑──⦁*
 
-1♻️➣ To put Antilink type :
+1⚙️🧬➣ To put Antilink type :
 Eg:- .setvar ANTILINK:false
 
-2♻️➣ To on/off Auto reaction type :
+2⚙️🧬➣ To on/off Auto reaction type :
 Eg:- .setvar AUTO_REACTION:false/true
 
-3♻️➣ To on/off Auto read status type : 
+3⚙️🧬➣ To on/off Auto read status type : 
 Eg:- .setvar AUTO_READ_STATUS:false/true
 
-4♻️➣ To on/off Auto status save type :
+4⚙️🧬➣ To on/off Auto status save type :
 Eg:- .setvar AUTO_STATUS_SAVER:false/true
 
-5♻️➣ To on/off heroku type :
+5⚙️🧬➣ To on/off heroku type :
 Eg:- .setvar heroku:false/true
 
-6♻️➣ To put Heroku api key type :
+6⚙️🧬➣ To put Heroku api key type :
 Eg:- .setvar HEROKU_API_KEY:put api key
 
-7♻️➣ To put Heroku app name type :
+7⚙️🧬➣ To put Heroku app name type :
 Eg:- .setvar HEROKU_APP_NAME:put app name
 
-8♻️➣ To on/off Auto reaction type :
+8⚙️🧬➣ To on/off Auto reaction type :
 Eg:- .setvar HEROKU_API_KEY:put api key
 
-9♻️➣ To on/off Auto reaction type :
+9⚙️🧬➣ To on/off Auto reaction type :
 Eg:- .setvar HEROKU_API_KEY:put api key
 
-10♻️➣ To on/off Level up message type :
+10⚙️🧬➣ To on/off Level up message type :
 Eg:- .setvar LEVEL_UP_MESSAGE:false/true
 
-11♻️➣ To put Mongodb url type :
+11⚙️🧬➣ To put Mongodb url type :
 Eg:- .setvar MONGODB_URI:put mongodb url
 
-12♻️➣ To put Open api key type :
+12⚙️🧬➣ To put Open api key type :
 Eg:- .setvar OPENAI_API_KEY:put open api key
 
-13♻️➣ To put Owner name type :
+13⚙️🧬➣ To put Owner name type :
 Eg:- .setvar OWNER_NUMBER:put any name
 
-14♻️➣ To put Owner number type :
+14⚙️🧬➣ To put Owner number type :
 Eg:- .setvar OWNER_NUMBER:92xxxxxxxx
 
-15♻️➣ To put Pack info type :
+15⚙️🧬➣ To put Pack info type :
 Eg:- .setvar PACK_INFO:put any name
 
-16♻️➣ To put prefix type :
+16⚙️🧬➣ To put prefix type :
 Eg:- .setvar PREFIX:.
 
-17♻️➣ To on/off Auto Read message type :
+17⚙️🧬➣ To on/off Auto Read message type :
 Eg:- .setvar READ_MESSAGE:false/true
 
-18♻️➣ To put thumb image type :
-Eg:- .set THUMB_IMAGE:put image url
-
-19♻️➣ To public/privert  type :
+18⚙️🧬➣ To public/privert  type :
 Eg:- .setvar WORKTYPE:public/private
 
 
-©𝙂𝙀𝙉𝙀𝙍𝘼𝙏𝙀𝘿 𝘽𝙔 𝙆𝙄𝙉𝙂-𝙈𝘿
+*©SETTINGS BY SAVIYA-MD*
 `,
 
                 footer: tlang().footer,
